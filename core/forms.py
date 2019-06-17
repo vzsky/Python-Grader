67edb87a1d4d87d45f858d2e1a287d04581ea8d1
sipp11 <sipp11@zzyzx.co>
2019-06-18 03:09:36 +0900

    Should be able to work now

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from Problems.models import Probs
 
-class Submission (forms.Form):
+
+class Submission(forms.Form):
     all_pb = Probs.objects.all()
     CHOICES = []
-    for each_pb in all_pb :
+    for each_pb in all_pb:
         Opt = (each_pb.id, each_pb.title)
         CHOICES.append(Opt)
     select_id = forms.ChoiceField(choices=CHOICES)
--- core/views.py
+++ core/views.py
@@ -3,32 +3,37 @@ from Problems.models import Probs
 from .forms import Submission
 from .Grade import Grade
 
+
 def home(request):
-    if request.method == 'POST' and request.user.is_authenticated:
+    if request.method == "POST" and request.user.is_authenticated:
 
         form = Submission(request.POST, request.FILES)
         print(form.errors)
         if form.is_valid():
-            code = request.FILES['uploaded_file']
-            codeid = form.cleaned_data['select_id']
-
-            try :
+            code = request.FILES["uploaded_file"]
+            codeid = form.cleaned_data["select_id"]
+            try:
                 prob = Probs.objects.get(pk=codeid)
                 Grade(code, prob, request.user)
-            except Exception as e :
+            except Exception as e:
                 print(e)
 
-            return redirect('home')
+            return redirect("home")
 
     all_pb = Probs.objects.all()
-
-    res = request.user.results.all()
+    res = []
+    user = request.user
+    if user.is_authenticated:
+        res = request.user.results.all()
     score = 0
 
-    for result in res :
+    for result in res:
         score += result.score
 
-    
-
     form = Submission()
-    return render(request, 'home.html', {"all_pb" : all_pb, "form" : form, "res" : res, "score" : score})
+    return render(
+        request,
+        "home.html",
+        {"all_pb": all_pb, "form": form, "res": res, "score": score},
+    )
+
